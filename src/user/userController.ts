cd5dd6c2e79169a547712a6352f8c04465411ed3
Rimjhim-Lalwani <rimjhim2361.be21@chitkara.edu.in>
2024-06-23 10:54:01 +0530

    Added user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser=async(
     req:Request,
@@ -22,6 +23,12 @@ const createUser=async(
         const error=createHttpError(400,"All fields are required");
         return next(error); //passes error to global error handler
 
+    }
+    //Database call
+    const user= await userModel.findOne({email});
+    if(user){
+        const error=createHttpError(400,"User already exists with this email");
+        return next(error);
     }
     //Process
     //Response
